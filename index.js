16c098e0097f88c381691397962aa4b5b2011a3b
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-10-05 21:31:47 +0200

    step 4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const persons = [
+let persons = [
   { id: 1, name: "Arto Hellas", number: "040-123456" },
   { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
   { id: 3, name: "Dan Abramov", number: "12-43-234345" },
@@ -31,6 +31,12 @@ app.get("/api/persons/:id", (req, res) => {
   }
   console.log(person);
   console.log(id, typeof id);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
 });
 
 const PORT = 3001;
