9581cf627d9cd3991630847ff44b5909e72226c1
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-10-05 21:22:44 +0200

    step3

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(express.json());
 
 const persons = [
-  { name: "Arto Hellas", number: "040-123456" },
-  { name: "Ada Lovelace", number: "39-44-5323523" },
-  { name: "Dan Abramov", number: "12-43-234345" },
-  { name: "Mary Poppendieck", number: "39-23-6423122" },
+  { id: 1, name: "Arto Hellas", number: "040-123456" },
+  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+  { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
 ];
 
 app.get("/api/persons", (req, res) => {
@@ -17,6 +17,20 @@ app.get("/api/persons", (req, res) => {
 app.get("/info", (req, res) => {
   const info = `Phonebook has info for ${persons.length} people ${new Date()}`;
   res.json(info);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => {
+    return person.id === id;
+  });
+  if (person) {
+    res.json(person);
+  } else {
+    return res.status(404).end("Person not found");
+  }
+  console.log(person);
+  console.log(id, typeof id);
 });
 
 const PORT = 3001;
